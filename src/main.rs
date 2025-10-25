dfd4c224553f9dd17157b6ab7293de67c0d804a8
William Emanuelsson <wem@kth.se>
2025-10-25 22:51:46 +0200

    Add board type

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
 use std::fmt;
 
+const BOARD_SIZE: usize = 8;
+const BOARD_SIZE_U8: u8 = BOARD_SIZE as u8;
 
+#[derive(Clone, Copy)]
 enum Color { White, Black }
 
+#[derive(Clone, Copy)]
 enum Kind { Pawn, Knight, Bishop, Rook, Queen, King }
 
 
@@ -28,6 +32,7 @@ impl fmt::Display for Kind {
     }
 }
 
+#[derive(Clone, Copy)]
 struct Piece {
     color: Color,
     kind: Kind
@@ -41,7 +46,38 @@ impl fmt::Display for Piece {
     }
 }
 
+struct Square(u8);
+
+impl Square {
+    fn from_coords(file: u8, rank: u8) -> Option<Self> {
+        (file < BOARD_SIZE_U8 && rank < BOARD_SIZE_U8)
+            .then(|| Self(rank * BOARD_SIZE_U8 + file))
+    }
+
+    fn idx(&self) -> usize {
+        self.0 as usize
+    }
+}
+
+struct Board {
+    squares: [Option<Piece>; 64],
+}
+
+impl Board {
+
+    fn empty() -> Self {
+        Self{ squares: [None; 64] }
+    }
+
+    fn place(&mut self, p: Piece, sq: Square){
+        self.squares[sq.idx()] = Some(p);
+    }
+}
+
 fn main() {
     let test_piece = Piece { color: Color::White, kind: Kind::King };
+    let square = Square::from_coords(3, 3).unwrap();
+    let mut board = Board::empty();
+    board.place(test_piece, square);
     println!("Hello, world! {}", test_piece);
 }
